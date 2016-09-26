fa87d83bcb768663d3e72f2382d7c6b7ad1400fb
Anthony Holt <Anthony Holt>
2016-09-26 08:28:26 -0400

    testing out how to throw up a website to the github.

---
+++ script.js
@@ -0,0 +1,10 @@
+ $(document).ready(function(){
+
+ }
+
+ function("cost"){
+ 	var cost = 10;
+ 	return = cost
+ }
+
+ .onclick("total" alert(function("cost") " This is a test site no actual check out will happen."))
